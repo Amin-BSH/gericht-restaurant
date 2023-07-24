cb673b8a01a66db5a71568b4d1fc25c1fcc353ba
Amin-BSH <m.aminbesharatian@gmail.com>
2023-07-24 14:34:38 +0330

    feat: complete the navbar component
    complete the navbar component and the CSS file for this component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,32 @@
-
+import {
+  AboutUs,
+  Chef,
+  FindUs,
+  Footer,
+  Gallery,
+  Header,
+  Intro,
+  Laurels,
+  SpecialMenu,
+} from "./container";
+import { Navbar } from "./components";
 import "./App.css";
 
 function App() {
-  return <></>;
+  return (
+    <div>
+      <Navbar />
+      <Header />
+      <AboutUs />
+      <SpecialMenu />
+      <Chef />
+      <Intro />
+      <Laurels />
+      <Gallery />
+      <FindUs />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
