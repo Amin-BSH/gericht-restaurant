0e98967865deccf70fb0cd145c55bf357245a72f
Amin-BSH <m.aminbesharatian@gmail.com>
2023-07-24 15:54:45 +0330

    build: install prop-types
    use prop-types in SubHeading component

--- src/components/SubHeading/SubHeading.jsx
+++ src/components/SubHeading/SubHeading.jsx
@@ -1,5 +1,7 @@
 import { images } from "../../constants";
+import PropTypes from "prop-types";
 
+// eslint-disable-next-line react/prop-types
 const SubHeading = ({ title }) => {
   return (
     <div style={{ marginBottom: "1rem" }}>
@@ -7,6 +9,10 @@ const SubHeading = ({ title }) => {
       <img src={images.spoon} alt="spoon" className="spoon__img" />
     </div>
   );
+};
+
+SubHeading.prototype = {
+  title: PropTypes.string.isRequired,
 };
 
 export default SubHeading;
