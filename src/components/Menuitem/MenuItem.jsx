efd0e17ae0b38263039fa273e7cfea5620c10084
Amin-BSH <m.aminbesharatian@gmail.com>
2023-07-25 07:26:26 +0330

    feat: complete the SpecialMenu and MenuItem component
    complete the SpecialMenu and MenuItem component and Css files for these components.
    
    use prop-types in MenuItem component

--- src/components/Menuitem/MenuItem.jsx
+++ src/components/Menuitem/MenuItem.jsx
@@ -1,11 +1,34 @@
-import React from 'react';
+import PropTypes from "prop-types";
+import "./MenuItem.css";
 
-import './MenuItem.css';
+const MenuItem = ({ title, price, tags }) => {
+  return (
+    <div className="app__menuitem">
+      <div className="app__menuitem-head">
+        <div className="app__menuitem-name">
+          <p className="p__cormorant" style={{ color: "#DCCA87" }}>
+            {title}
+          </p>
+        </div>
+        <div className="app__menuitem-dash" />
+        <div className="app__menuitem-price">
+          <p className="p__cormorant">{price}</p>
+        </div>
+      </div>
 
-const MenuItem = () => (
-  <div>
-    MenuItem
-  </div>
-);
+      <div className="app__menuitem-sub">
+        <p className="p__opensans" style={{ color: "#AAAAAA" }}>
+          {tags}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+MenuItem.prototype = {
+  title: PropTypes.string.isRequired,
+  price: PropTypes.string.isRequired,
+  tags: PropTypes.string.isRequired,
+};
 
 export default MenuItem;
